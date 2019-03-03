60506202c7b87b3ed7c230d234c2c371f4827aad
chenminglin <806663326@qq.com>
2019-03-03 23:51:45 +0800

    首次提交

---
+++ app/src/main/java/com/bethena/submodulestudy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bethena.submodulestudy;
+
+import android.os.Bundle;
+
+import com.bethena.common.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
